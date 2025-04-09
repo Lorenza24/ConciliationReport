4c72c9a310ee380d4e207a81389128ef0197dea9
Loren <lorenda9824@gmail.com>
2025-04-09 08:48:09 -0500

    point 1

--- normalization/dota/points/point_1.py
+++ normalization/dota/points/point_1.py
@@ -18,5 +18,5 @@ def point_1(df):
     # Construct the masked card number by combining the first six digits,
     # masking the middle six digits with 'XXXXXX', and appending the last four digits
     df_new["CARD_NUMBER"] = df["CARD_SIX_FIRST_DIGITS"].astype(str) + "XXXXXX" + df["CARD_FOUR_LAST_DIGITS"].astype(str)
-
+    print("me siento bien")
     return df_new
